25e53d83804ac167ffcf46bdd1fa8180659d6c4a
Jacob Ross <jacobkarmelross@gmail.com>
2021-10-09 17:24:36 +0200

    feat(server boiler plat and mongoose boiler plate): server boiler plat and mongoose boiler plate

--- src/DB/connection.ts
+++ src/DB/connection.ts
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+
+async function connect() {
+  try {
+    await mongoose.connect(`mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+    console.log('db connected');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = { connect };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 
-// require('dotenv').config();
 const express = require('express');
+const { connect } = require('./DB/connection.ts');
 
 const app = express();
+
+// will changed as DB is developed but for now allows basic set up
+connect();
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`listening on port ${process.env.SERVER_PORT}`);
