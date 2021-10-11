d05c90f854dc2b971f8218f616c4d8054acfa7c3
chrisbohne <christoph.bohne90@gmail.com>
2021-10-11 12:19:51 +0200

    add Login/Signup Component

--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -1,7 +1,10 @@
+import Login from '../Login/Login';
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Login />
     </div>
   );
 }
--- client/src/Components/Login/Login.tsx
+++ client/src/Components/Login/Login.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Inputs = {
+  username: string;
+  password: string;
+};
+
 export default function Login() {
+  const [isLogin, setIsLogin] = useState(true);
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="Username" {...register('username')} />
+        {!isLogin && <input placeholder="Email" />}
+        <input placeholder="Password" {...register('password')} />
+        {!isLogin && <input placeholder="Confirm Password" />}
+        <input type="submit" value={isLogin ? 'Login' : 'Sign In'} />
+      </form>
+      <button type="button" onClick={() => setIsLogin(!isLogin)}>
+        {isLogin ? 'Not Signed up yet? Sign up' : 'Already Signed up? Login'}
+      </button>
     </div>
   );
 }
