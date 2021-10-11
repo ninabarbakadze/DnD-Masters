246ce3ab3d629e92ff82bbe8b3450e9477774e01
chrisbohne <christoph.bohne90@gmail.com>
2021-10-11 10:54:17 +0200

    add Router and update prettierrc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;
