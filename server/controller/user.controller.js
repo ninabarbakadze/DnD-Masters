783d5be7f612773f9ee1b951e6d1f03ecf749d4a
David Rozsavolgyi <muraminsav@gmail.com>
2021-10-11 12:40:49 +0100

    db fix

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -2,14 +2,17 @@ const User = require('../DB/models/user.model');
 
 exports.createUser = async (req, res) => {
   try {
-    const { userName } = req.body;
+    const { userName } = await req.body;
     console.log(userName);
 
     const userExist = await User.find({ userName });
     console.log(userExist);
-    if (userExist) throw new Error('User already exist');
-    // await User.create(newUser);
-    res.status(200).send({ message: 'new user created' });
+    if (userExist.length > 0) {
+      throw new Error('User already exist');
+    } else {
+      await User.create(req.body);
+      res.status(200).send({ message: 'new user created' });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
