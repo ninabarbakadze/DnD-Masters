d827d83fb0d75288cf227a4679f3c2382ae5660d
David Rozsavolgyi <muraminsav@gmail.com>
2021-10-11 15:12:19 +0100

    user.controller refactor

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -2,14 +2,11 @@ const User = require('../DB/models/user.model');
 
 exports.createUser = async (req, res) => {
   try {
-    const { userName } = await req.body;
-    const userExist = await User.exists({ userName });
-    if (userExist) {
-      throw new Error('User already exist');
-    } else {
-      await User.create(req.body);
-      res.status(200).send({ message: 'new user created' });
-    }
+    const newUser = await req.body;
+    const userExist = await User.exists({ userName: newUser.userName });
+    if (userExist) throw new Error('User already exist');
+    await User.create(newUser);
+    res.status(200).send({ message: 'new user created' });
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
