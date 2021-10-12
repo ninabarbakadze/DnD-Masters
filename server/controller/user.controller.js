52cde3fa5714bc318e4984ce5f7ee6e886320c66
David Rozsavolgyi <muraminsav@gmail.com>
2021-10-12 18:46:50 +0100

    fetched

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,14 +1,49 @@
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const User = require('../DB/models/user.model');
 
-exports.createUser = async (req, res) => {
+const logIn = (req, res, next) => {
   try {
-    const newUser = req.body;
-    const userExist = await User.exists({ email: newUser.email });
-    if (userExist) res.status(403).json({ error: 'User already exist' });
-    await User.create(newUser);
-    res.status(200).send({ message: 'new user created' });
-  } catch (error) {
-    console.log(error);
-    res.status(500);
+    passport.authenticate('local', (err, user) => {
+      if (err) throw err;
+      if (!user) res.send({ status: 'No User Exists' });
+      else {
+        req.logIn(user, (error) => {
+          if (error) throw error;
+          res.send({ status: 'Successfully Authenticated' });
+        });
+      }
+    })(req, res, next);
+  } catch (err) {
+    res.status(500).send(err);
   }
 };
+
+const register = (req, res) => {
+  try {
+    User.findOne({ username: req.body.username }, async (err, user) => {
+      if (err) throw err;
+      if (user) {
+        res.send({ status: 'User Already Exists' });
+      } else {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = new User({
+          username: req.body.username,
+          password: hashedPassword,
+        });
+        await newUser.save();
+        res.status(200);
+        res.send({ status: 'User created' });
+      }
+    });
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+};
+
+const getUser = (req, res) => {
+  res.send(req.user); // <--- req.user stires the entire authenticated user
+};
+
+module.exports = { register, logIn, getUser };
