83b6940a8cb86303dd2a25a2f59ba036d1b94db8
chrisbohne <christoph.bohne90@gmail.com>
2021-10-13 16:21:53 +0200

    create basic carousel component

--- client/src/Components/MapWizard/MapWizard.tsx
+++ client/src/Components/MapWizard/MapWizard.tsx
@@ -3,10 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 import MapSelection from './Steps/MapSelection';
 import MapUpload from './Steps/MapUpload';
 import MapEdit from './Steps/MapEdit';
+import './MapWizard.scss';
 
 export default function MapWizard() {
   return (
-    <div>
+    <div className="map-wizard-container">
       <h1>Map Wizard</h1>
       <Switch>
         <Route path="/mapWizard/mapSelection">
