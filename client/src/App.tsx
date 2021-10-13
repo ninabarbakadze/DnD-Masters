866f28d73ebbb0c199609b4acfef41d1c3ae25cf
chrisbohne <christoph.bohne90@gmail.com>
2021-10-13 17:21:56 +0200

    add selected map feature on carousel for mapwizard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
-import GameWizard from './Components/GameWizard/GameWizard';
+import GameWizard from './Components/GameWizard/Gamewizard';
 import CharacterWizard from './Components/CharacterWizard/CharacterWizard';
+import MapWizard from './Components/MapWizard/MapWizard';
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
         </Route>
         <Route path="/characterWizard">
           <CharacterWizard />
+        </Route>
+        <Route path="/mapWizard">
+          <MapWizard />
         </Route>
       </Switch>
     </Router>
--- client/src/Components/MapWizard/Steps/MapSelection.tsx
+++ client/src/Components/MapWizard/Steps/MapSelection.tsx
@@ -1,36 +1,27 @@
 import { useState } from 'react';
 import Carousel from '../../Carousel/Carousel';
+import MapCarouselItem from '../MapCarouselItem/MapCarouselItem';
 import './MapSteps.scss';
 
 export default function MapSelection() {
   const [mapIndex, setMapIndex] = useState(0);
+  const maps = [
+    <MapCarouselItem name="Box1" />,
+    <MapCarouselItem name="Box2" />,
+    <MapCarouselItem name="Box3" />,
+    <MapCarouselItem name="Box4" />,
+    <MapCarouselItem name="Box5" />,
+    <MapCarouselItem name="Box6" />,
+  ];
   return (
     <div>
-      <h1>
-        Map Name:
-        {mapIndex}
-      </h1>
-      <h2>Map Image</h2>
+      <h1>{maps[mapIndex].props.name}</h1>
+      <div>{maps[mapIndex]}</div>
       <Carousel setMapIndex={setMapIndex} show={3}>
-        <div className="box-container">
-          <div className="box">Box1</div>
-        </div>
-        <div className="box-container">
-          <div className="box">Box2</div>
-        </div>
-        <div className="box-container">
-          <div className="box">Box3</div>
-        </div>
-        <div className="box-container">
-          <div className="box">Box4</div>
-        </div>
-        <div className="box-container">
-          <div className="box">Box5</div>
-        </div>
-        <div className="box-container">
-          <div className="box">Box6</div>
-        </div>
+        {maps}
       </Carousel>
+      <button type="button">Edit Map</button>
+      <button type="button">Delete Map</button>
     </div>
   );
 }
