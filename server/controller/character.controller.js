d1c1ca3adb771dfa122af303be2b163b474c98f9
David Rozsavolgyi <muraminsav@gmail.com>
2021-10-19 07:38:38 +0100

    pull req

--- server/controller/character.controller.js
+++ server/controller/character.controller.js
@@ -1,2 +1,10 @@
 const User = require('../DB/models/user.model');
 const Character = require('../DB/models/user.model');
+
+// const getAllCharacters = async (req, res)=>{
+//     try {
+
+//     } catch (error) {
+
+//     }
+// }
